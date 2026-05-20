eec595fab1a0c20fcd8d8365be8957f713b5f2dc
Avery <averywoodlief@gmail.com>
2026-05-20 16:23:05 -0500

    fixed: bug where wrong cursor crashed program if left mouse button clicked

--- code/main.py
+++ code/main.py
@@ -13,11 +13,12 @@ while running:
         if (event.type == pygame.MOUSEBUTTONDOWN):
             if (event.button == 1):
                 #print("left click")
-                mouseX, mouseY = pygame.mouse.get_pos()
-                cell_from_mouse = display.grid.euclid_to_cell((mouseX, mouseY))
+                if (pygame.mouse.get_cursor() == display.images["cursor swapper"]):
+                    mouseX, mouseY = pygame.mouse.get_pos()
+                    cell_from_mouse = display.grid.euclid_to_cell((mouseX, mouseY))
 
-                selected_block = display.grid[cell_from_mouse]
-                display.grid.swap_block_with_right_neighbor(selected_block)
+                    selected_block = display.grid[cell_from_mouse]
+                    display.grid.swap_block_with_right_neighbor(selected_block)
 
 
     #print(pygame.mouse.get_pos())
