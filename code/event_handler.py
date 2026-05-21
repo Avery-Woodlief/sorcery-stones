2fd4f19ebba8a1fcdc47fb152d0cd9b07e21f00c
Avery <averywoodlief@gmail.com>
2026-05-21 16:34:42 -0500

    cascade function

--- code/event_handler.py
+++ code/event_handler.py
@@ -45,36 +45,6 @@ def handle_events(event, display):
                 cell_from_mouse = display.grid.euclid_to_cell((mouseX, mouseY))
                 display.grid.swap_block_with_right_neighbor(cell_from_mouse)
                 execute_matches_until_stable(display, cell_from_mouse)
-                #selected_block = display.grid[cell_from_mouse]
-                '''
-                try:
-                    display.grid.swap_block_with_right_neighbor(cell_from_mouse)
-                    r1,c1=cell_from_mouse
-
-                    row_matching_blocks = [(r1, c1 + 1)]
-                    row_match_query(display.grid, (r1, c1 + 1), row_matching_blocks, 1)
-                    if (len(list(set(row_matching_blocks))) >= 3):
-                        display.grid.del_blocks(list(set(row_matching_blocks)))
-
-                    row_matching_blocks = [(r1, c1)]
-                    row_match_query(display.grid, (r1, c1), row_matching_blocks, 1)
-                    if (len(list(set(row_matching_blocks))) >= 3):
-                        display.grid.del_blocks(list(set(row_matching_blocks)))
-
-                    col_matching_blocks = [(r1, c1 + 1)]
-                    column_match_query(display.grid, (r1, c1 + 1), col_matching_blocks, 1)
-                    if (len(list(set(col_matching_blocks))) >= 3):
-                        display.grid.del_blocks(list(set(col_matching_blocks)))
-
-                    col_matching_blocks = [(r1, c1)]
-                    column_match_query(display.grid, (r1, c1), col_matching_blocks, 1)
-                    if (len(list(set(col_matching_blocks))) >= 3):
-                        display.grid.del_blocks(list(set(col_matching_blocks)))
-
-                
-                except (ValueError) as e:
-                    return # nothing to do
-                '''
 
                 
 
