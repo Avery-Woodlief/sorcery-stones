72109b7011214fb3c009d296ca74f403375d2022
Avery <averywoodlief@gmail.com>
2026-06-04 13:05:41 -0500

    added some new classes

---
+++ code/announcer.py
@@ -0,0 +1,14 @@
+class Announcer:
+
+    def __init__(self, player):
+        self.participant = player
+        self.messages = [
+                            "Level up!",
+                            "Cool!",
+                            "Nice!",
+                            "Great!",
+                            "Wow!",
+                            "Time is running out!",
+                            "You win!",
+                            "Better luck next time!"
+                        ]
---
+++ code/player.py
@@ -0,0 +1,7 @@
+class Player:
+
+    def __init__(self, player_name):
+        self.name = player_name
+        self.score = 1
+        self.current_level = 0
+        self.is_alive = True
